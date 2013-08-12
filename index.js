55be97995d81c1c614405e5f4ebbb97d0915bce9
Ben McMahen <ben.mcmahen@gmail.com>
2013-08-12 15:34:24 -0600

    spacing

--- index.js
+++ index.js
@@ -9,49 +9,49 @@ module.exports = Hold;
  */
 
 function Hold(el, fn){
-	if (!(this instanceof Hold)) return new Hold(el, fn);
-	this.el = el;
-	this.fn = fn || function() {};
-	this.events = events(this.el, this);
-	this.events.bind('mousedown', 'ontouchstart');
-	this.events.bind('mouseup', 'ontouchend');
-	this.events.bind('touchstart');
-	this.events.bind('touchend');
+  if (!(this instanceof Hold)) return new Hold(el, fn);
+  this.el = el;
+  this.fn = fn || function() {};
+  this.events = events(this.el, this);
+  this.events.bind('mousedown', 'ontouchstart');
+  this.events.bind('mouseup', 'ontouchend');
+  this.events.bind('touchstart');
+  this.events.bind('touchend');
 }
 
 // consider making 'touchleave' its own module.
 Hold.prototype.bindLeave = function(){
-	this.events.bind('mouseout', 'ontouchend');
-	this.events.bind('touchmove');
+  this.events.bind('mouseout', 'ontouchend');
+  this.events.bind('touchmove');
 };
 
 Hold.prototype.unbindLeave = function(){
-	this.events.unbind('mouseleave', 'ontouchend');
-	this.events.unbind('touchmove');
+  this.events.unbind('mouseleave', 'ontouchend');
+  this.events.unbind('touchmove');
 }
 
 Hold.prototype.ontouchmove = function(e){
-	var touch = e.touches[0];
-	var element = document.elementFromPoint(touch.pageX, touch.pageY);
-	if (element !== this.el) {
-		this.ontouchend();
-	}
+  var touch = e.touches[0];
+  var element = document.elementFromPoint(touch.pageX, touch.pageY);
+  if (element !== this.el) {
+    this.ontouchend();
+  }
 };
 
 Hold.prototype.ontouchstart = function(e){
-	e.preventDefault();
-	this.bindLeave();
-	var self = this;
-	this.timeout = setTimeout(function(){
-		self.fn(e);
-	}, 800);
+  e.preventDefault();
+  this.bindLeave();
+  var self = this;
+  this.timeout = setTimeout(function(){
+    self.fn(e);
+  }, 800);
 };
 
 Hold.prototype.ontouchend = function(e){
-	clearTimeout(this.timeout);
-	this.unbindLeave();
+  clearTimeout(this.timeout);
+  this.unbindLeave();
 };
 
 Hold.prototype.unbind = function(){
-	this.events.unbind();
+  this.events.unbind();
 };
